7473e00092c3b648729ef516a35e4775ab7d362c
Prajwal0099 <prajwaldesai920@gmail.com>
2024-03-16 00:04:32 +0530

    Initial Commit

---
+++ src/main/java/com/bankLendingPortal/BankLendingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.bankLendingPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLendingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLendingPortalApplication.class, args);
+	}
+
+}
